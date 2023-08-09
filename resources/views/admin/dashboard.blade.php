50089dbaba203e6fd2fe0b94d19a1b02a96fff63
CraciDenisStefan <crdenis13@gmail.com>
2023-08-09 12:20:27 +0200

    Modificato stile

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
+
+    <div class="row justify-content-center my-5">
         <div class="col">
             <div class="card">
                 <div class="card-header">{{ __('User Dashboard') }}</div>
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
 
-Index progetti
+<div class="m-5">
+    <table class="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Data</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Apri</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach($projects as $project)
+          <tr >
+            <th scope="row">{{$project->id}}</th>
+            <td>{{$project->title}}</td>
+            <td>{{$project->date}}</td>
+            <td>${{$project->description}}</td>
+            <td class="text-center">
+              <a href="#"><i class="fa-solid fa-eye"></i></a>
+              <a href="#"><i class="fa-solid fa-pen-to-square text-warning"></i></a>
+              {{-- <form action="{{route('comics.destroy', $comic->id)}}" method="POST" onsubmit="return confirm('Sei sicuro di voler cancellare ?!?!?!?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn text-danger">
+                  <i class="fa-solid fa-trash-can"></i>
+                </button>
+              </form> --}}
+            </td>
+          </tr>
+        </tbody>
+        @endforeach
+      </table>
+
+</div>
 
 @endsection
