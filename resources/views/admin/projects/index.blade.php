35eb0c9b8e625285effa6771dd04f367f5116bbb
CraciDenisStefan <crdenis13@gmail.com>
2023-08-21 15:21:03 +0200

    Esercizio completato

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="m-5">
+<div class="container">
 <div class="col-12 col-sm-6 col-md-3 my-5">
   <a href="{{route('admin.projects.create')}}" class="btn btn-primary">Aggiungi progetto</a>
 </div>
@@ -13,8 +13,8 @@
             <th scope="col">ID</th>
             <th scope="col">Titolo</th>
             <th scope="col">Data</th>
-            <th scope="col">Descrizione</th>
             <th scope="col">Apri</th>
+            <th scope="col">Modifica/Elimina</th>
           </tr>
         </thead>
         <tbody>
@@ -23,17 +23,18 @@
             <th scope="row">{{$project->id}}</th>
             <td>{{$project->title}}</td>
             <td>{{$project->date}}</td>
-            <td>{{$project->description}}</td>
-            <td class="text-center">
-              <a href="{{route('admin.projects.show', $project->id)}}"><i class="fa-solid fa-eye"></i></a>
-              <a href="{{route('admin.projects.edit', $project->id)}}"><i class="fa-solid fa-pen-to-square text-warning"></i></a>
-              {{-- <form action="{{route('comics.destroy', $comic->id)}}" method="POST" onsubmit="return confirm('Sei sicuro di voler cancellare ?!?!?!?')">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn text-danger">
-                  <i class="fa-solid fa-trash-can"></i>
-                </button>
-              </form> --}}
+            <td><a href="{{route('admin.projects.show', $project->id)}}"><i class="fa-solid fa-eye"></i></a></td>
+            <td>
+              <div class="d-flex">
+                  <a href="{{route('admin.projects.edit', $project->id)}}"><i class="fa-solid fa-pen-to-square text-warning"></i></a>
+                  <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST" onsubmit="return confirm('Sei sicuro di voler cancellare ?!?!?!?')">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn text-danger">
+                      <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                  </form>
+              </div>
             </td>
           </tr>
         </tbody>
